63a8f3304bacc151ff67e66820a91cbf42682c4a
Liz <jesusridesthebus@gmail.com>
2019-05-31 10:02:21 -0700

    add linting and bootstrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { ageCalculator } from './age-calculator.js';
 
 //$(document).ready(function() {
