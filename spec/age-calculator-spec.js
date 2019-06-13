0b0e543f08fdef46902e367da29aef51aed668da
Liz <jesusridesthebus@gmail.com>
2019-06-12 21:10:47 -0700

    add passing tests for age conversion

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -3,27 +3,36 @@ import { AgeCalculator } from './../src/age-calculator.js';
 describe('AgeCalculator', () => {
   it('should test whether birthday is converted to earth years', () => {
     let ageCalculator = new AgeCalculator("April 27, 1987");
-    console.log(ageCalculator);
-    // let earthAge = ageCalculator.earthAge;
-    expect(ageCalculator.getAge("Earth")).toEqual(32);
+    let earthAge = ageCalculator.getAge("Earth");
+    expect(earthAge).toEqual(32);
   });
 
-  it('should test whether correct age on Mercury is returned', () => {
+  it('should test whether the correct age on Mercury is returned', () => {
     let ageCalculator = new AgeCalculator("April 27, 1987");
     let mercuryAge = ageCalculator.getAge("Mercury");
     expect(mercuryAge).toEqual(133);
   });
 
-  it('should test whether the right age on any planet is returned', () => {
+  it('should test whether the correct age on Venus is returned', () => {
     let ageCalculator = new AgeCalculator("April 27, 1987");
     let ageOnVenus = ageCalculator.getAge("Venus");
     expect(ageOnVenus).toEqual(51);
   });
 
+  it('should test whether the correct age on Mars is returned', () => {
+    let ageCalculator = new AgeCalculator("April 27, 1987");
+    let ageOnMars = ageCalculator.getAge("Mars");
+    expect(ageOnMars).toEqual(17);
+  });
+
+  it('should test whether the correct age on Jupiter is returned', () => {
+    let ageCalculator = new AgeCalculator("April 27, 1987");
+    let ageOnJupiter = ageCalculator.getAge("Jupiter");
+    expect(ageOnJupiter).toEqual(2);
+  });
+
 
   //remaining years on earth
-
-  //remaining years on any planet
 
   //you should have died xx years ago
 });
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -5,9 +5,9 @@ export class AgeCalculator {
     this.planetConversion = {
       "Mercury": 0.24,
       "Venus": 0.62,
+      "Earth": 1,
       "Mars": 1.88,
-      "Jupiter": 11.86,
-      "Earth": 1
+      "Jupiter": 11.86
     }
   }
 
