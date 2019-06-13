9b4391ae185e38d3466445ec6cbff0c1a43bfbe2
Student <example@example.com>
2019-06-12 17:20:40 -0700

    alter getAge function, tests pass

--- spec/age-calculator-spec.js
+++ spec/age-calculator-spec.js
@@ -2,20 +2,22 @@ import { AgeCalculator } from './../src/age-calculator.js';
 
 describe('AgeCalculator', () => {
   it('should test whether birthday is converted to earth years', () => {
-    let ageCalculator = new AgeCalculator("1987-04-27");
-    let earthAge = ageCalculator.earthAge;
-    expect(earthAge).toEqual(32);
+    let ageCalculator = new AgeCalculator("April 27, 1987");
+    console.log(ageCalculator);
+    // let earthAge = ageCalculator.earthAge;
+    expect(ageCalculator.getAge("Earth")).toEqual(32);
   });
 
   it('should test whether correct age on Mercury is returned', () => {
-    let mercuryAge = ageCalculator.getAgeOnMercury();
+    let ageCalculator = new AgeCalculator("April 27, 1987");
+    let mercuryAge = ageCalculator.getAge("Mercury");
     expect(mercuryAge).toEqual(133);
   });
 
   it('should test whether the right age on any planet is returned', () => {
-    let ageCalculator = new AgeCalculator("1987-04-27");
-    let ageOnMercury = ageCalculator.getAgeOnAnyPlanet("Mercury");
-    expect(ageOnMercury).toEqual(133);
+    let ageCalculator = new AgeCalculator("April 27, 1987");
+    let ageOnVenus = ageCalculator.getAge("Venus");
+    expect(ageOnVenus).toEqual(51);
   });
 
 
--- src/age-calculator.js
+++ src/age-calculator.js
@@ -1,19 +1,20 @@
 /* eslint-disable no-unused-vars */
 export class AgeCalculator {
   constructor(birthday) {
-    this.birthday = birthday;
-    this.earthAge = earthAge;
+    this.birthday = new Date(birthday);
     this.planetConversion = {
-      "Mercury": 0.24, 
+      "Mercury": 0.24,
       "Venus": 0.62,
       "Mars": 1.88,
-      "Jupiter": 11.86
+      "Jupiter": 11.86,
+      "Earth": 1
     }
   }
 
-  getAge(birthday) {
-    let earthAge = (((Date.now() - (new Date(birthday).getTime())) / 31557600000).MathFloor); 
-    return earthAge;
+  getAge(planet) {
+    console.log(new Date().getTime() - this.birthday.getTime());
+    let earthAge = Math.floor(((new Date().getTime() - (this.birthday).getTime()) / 31557600000));
+    return Math.floor(earthAge / this.planetConversion[planet]);
   }
 
   // remainingYears() {
